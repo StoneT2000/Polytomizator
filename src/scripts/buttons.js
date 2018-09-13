ccc3ac03ce5376a0625d978730daca49ff97b552
StoneT2000 <stoneztao@gmail.com>
2018-09-13 23:37:24 +0800

    Updated mini

--- src/scripts/buttons.js
+++ src/scripts/buttons.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
   $("#redo").css("cursor", "not-allowed");
 
   $("#pointBrush").addClass("active");
-  console.log("Polytomizator v60")
+  console.log("Polytomizator v61")
 
   //Default values
   $("#grid_accuracy").val(20)
